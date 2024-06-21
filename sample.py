bb5720aee1cb7b33d6321be63550666a4890d549
Uttam <129719033+uttambodara@users.noreply.github.com>
2024-06-21 17:26:13 +0530

    sample.py

---
+++ sample.py
@@ -0,0 +1,6 @@
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('book/<int:id>/', views.book_detail, name='book_detail'),
+    path('catalog/', include('catalog.urls')),
+    re_path(r'^([0-9]+)/$', views.best),
+]
