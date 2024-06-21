be286b6571fadb46a2c2e2b720e88fc9774eef77
Uttam <129719033+uttambodara@users.noreply.github.com>
2024-06-21 17:27:36 +0530

    views.py

---
+++ views.py
@@ -0,0 +1,9 @@
+# filename: views.py (Django view functions)
+
+from django.http import HttpResponse
+
+def index(request):
+    # Get an HttpRequest - the request parameter
+    # perform operations using information from the request.
+    # Return HttpResponse
+    return HttpResponse('Hello from Django!')
